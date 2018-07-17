4297e4b1db5a72c82e990aff4cb5f4a7d917b82a
taowuT <3374608165@qq.com>
2018-07-17 22:16:42 +0800

    remove watch to computed

---
+++ src/components/Back.vue
@@ -0,0 +1,71 @@
+<template>
+  <transition name="back">
+    <div class="card-back-wrap" :style="{background: changeColor.color}" v-show="isBack">
+      <div class="back-black"></div>
+      <div class="back-line">
+        {{cvc}}
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props:{
+    changeColor: {
+      type: Object,
+      default: {}
+    },
+    isBack: {
+      type: Boolean,
+      default: false
+    },
+    cvc: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang='stylus' rel='stylesheet/stylus'>
+.card-back-wrap
+    position: absolute
+    top: 30px
+    left: 50%
+    margin-left: -145px
+    padding: 20px
+    box-sizing: border-box
+    width: 290px
+    height: 183px
+    background-color #999
+    border-radius: 14.5px
+    transition-timing-function: linear
+    overflow: hidden
+    .back-black
+      position: absolute
+      top: 9%
+      left: 0
+      width: 100%
+      height: 22%
+      background-color: #2a1d16
+    .back-line
+      position: absolute
+      top: 35%
+      left: 5%
+      box-sizing: border-box
+      padding-right: 5px
+      width: 75%
+      height: 18%
+      background-image: repeating-linear-gradient(0.1deg, #fff 20%, #fff 40%, #fea 40%, #fea 44%, #fff 44%)
+      text-align: right
+      line-height: 2.1
+
+.back-enter-active
+  transition: all .3s .3s
+.back-leave-active
+  transition: all .3s 
+.back-enter,
+.back-leave-active
+  transform: rotateY(-90deg)
+</style>
--- src/components/Tip.vue
+++ src/components/Tip.vue
@@ -1,53 +1,50 @@
 <template>
-    <transition name="tip">
-        <div class="tip-wrap" v-show="isTip">
-            <div class="tip">
-                {{tipText}}
-            </div>
-        </div>
-    </transition>
+  <transition name="tip">
+    <div class="tip-wrap" v-show="isTip">
+      <div class="tip">
+        {{tipText}}
+      </div>
+    </div>
+  </transition>
 </template>
 
 <script>
 export default {
-    props: {
-        tipText: {
-            type: String,
-            default: ''
-        },
-        isTip: {
-            type: Boolean,
-            default: false
-        }
+  props: {
+    tipText: {
+      type: String,
+      default: ''
     },
-    methods: {
-        
+    isTip: {
+      type: Boolean,
+      default: false
     }
+  }
 }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
 .tip-wrap 
-    position: fixed
-    left: 0
-    right: 0
-    top: 0
-    bottom: 0
-    background-color rgba(200, 200, 200, .5)
-    z-index: 998
-    .tip 
-        position: absolute 
-        top: 50%
-        left: 50%
-        transform: translate(-50%, -50%)
-        padding: 5px
-        max-width: 200px
-        color: #007bff 
-        font: 400 20px "微软雅黑"
-        text-align: center
-        border: 1px solid #007bff
-        border-radius: 5px
-        background-color: rgba(200, 200, 200, .5)
+  position: fixed
+  left: 0
+  right: 0
+  top: 0
+  bottom: 0
+  background-color rgba(200, 200, 200, .5)
+  z-index: 998
+  .tip 
+    position: absolute 
+    top: 50%
+    left: 50%
+    transform: translate(-50%, -50%)
+    padding: 5px
+    max-width: 200px
+    color: #007bff 
+    font: 400 20px "微软雅黑"
+    text-align: center
+    border: 1px solid #007bff
+    border-radius: 5px
+    background-color: rgba(200, 200, 200, .5)
 
 
 .tip-enter-active,
